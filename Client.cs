261ed4cf141067338e2bbd0ef9a7e6fccdb755ca
Opylus <oliver.olmaru@gmail.com>
2018-01-29 20:06:47 +0200

    Initial commit

---
+++ Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebSocketSharp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class Client
+    {
+        public Client()
+        {
+            using (var ws = new WebSocket("ws://localhost:46495/Laputa"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine("Laputa says: " + e.Data);
+
+                ws.Connect();
+                ws.Send("BALUS");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace ServerApp
+{
+    class Server
+    {
+        WebSocketServer wssv;
+        public Server()
+        {
+            wssv = new WebSocketServer(46495);
+            wssv.AddWebSocketService<Laputa>("/Laputa");
+            wssv.Start();
+
+            if (wssv.IsListening)
+            {
+                Console.WriteLine("Listening on port {0}, and providing WebSocket services:", wssv.Port);
+                foreach (var path in wssv.WebSocketServices.Paths)
+                    Console.WriteLine("- {0}", path);
+            }
+
+        }
+
+        public void CleanUp()
+        {
+            wssv.Stop();
+        }
+
+        public class Laputa : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                var msg = e.Data == "BALUS"
+                          ? "I've been balused already..."
+                          : "I'm not available now.";
+
+                Send(msg);
+            }
+        }
+    }
+}
