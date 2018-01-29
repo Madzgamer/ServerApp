628d11d6e25afcb1c67d36f700a9650d5c168123
Opylus <oliver.olmaru@gmail.com>
2018-01-29 20:34:08 +0200

    fixed

--- Client.cs
+++ Client.cs
@@ -11,13 +11,7 @@ namespace ServerApp
     {
         public Client()
         {
-
-<<<<<<< HEAD
-
             using (var ws = new WebSocket("ws://localhost:46495/Chat"))
-=======
-            using (var ws = new WebSocket("ws://localhost:46495/Laputa"))
->>>>>>> 709515df4dbd562c2c6e46a32e7d4d19a8db7aca
             {
                 ws.OnMessage += (sender, e) =>
                     Console.WriteLine("Oliver says: " + e.Data);
