0a66e6feef431f2022e1b4c4b2dde3942530c7ab
Opylus <oliver.olmaru@gmail.com>
2018-01-29 21:39:29 +0200

    BIG F*CKING UPDATE

---
+++ Datastructures.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class Datastructures
+    {
+    }
+
+
+    [Serializable]
+    public class ChatMessages
+    {
+        public List<Message> messages { get; set; }
+        public ChatMessages(List<Message> messages)
+        {
+            this.messages = messages;
+        }
+
+
+        public ChatMessages()
+        {
+            messages = new List<Message>();
+        }
+
+        public void AddMessage(string message)
+        {
+            messages.Add(new Message(message));
+        }
+
+        public class Message
+        {
+            public Message(string message)
+            {
+                this.message = message;
+                createdAt = DateTime.Now;
+                creator = -1;
+            }
+
+            public DateTime createdAt { get; set; }
+            public string message { get; set; }
+            public int creator { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ServerApp
 {
@@ -11,10 +14,31 @@ namespace ServerApp
     {
         static void Main(string[] args)
         {
-            Server server = new Server();
-            Console.ReadKey();
-            Thread.Sleep(1000);
-            Client client = new Client();
+            ChatMessages messages = new ChatMessages();
+
+            messages.AddMessage("Tere");
+            messages.AddMessage("Head aega");
+            messages.AddMessage("Mul");
+            messages.AddMessage("Pole");
+            messages.AddMessage("Teile");
+            messages.AddMessage("Aega");
+
+            string data = JsonConvert.SerializeObject(messages);
+            Console.WriteLine(data);
+
+            ChatMessages receivedMessages = JsonConvert.DeserializeObject<ChatMessages>(data);
+
+            foreach(var message in receivedMessages.messages)
+            {
+                Console.WriteLine(message.message);
+            }
+
+
+            Console.ReadLine();
+            //server server = new server();
+            //console.readkey();
+            //thread.sleep(1000);
+            //client client = new client();
 
         }
     }
