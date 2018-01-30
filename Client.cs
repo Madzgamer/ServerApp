4bb89a9852d1235dee3194e9d85ea1117ed48de6
aged333 <randal.annus@gmail.com>
2018-01-30 15:48:40 +0200

    client

--- Client.cs
+++ Client.cs
@@ -11,19 +11,46 @@ namespace ServerApp
     {
         public Client()
         {
-            // requestId, saab id
+            // CreateWebSocket()
+            // CheckConnection(), server returnib "Here" et kindlaks teha kas serveriga on ühendus
+            // RequestId, saab id
             // loop
-            // getNewMessages, messagesObject
+            // GetNewMessages, messagesObject
             //
             //oliveri ip  84.50.187.110
-            using (var ws = new WebSocket("ws://localhost:46495/Chat"))
+            //port 46495
+
+            run(); //tegelt peaks seda väljaspoolt tegema
+        }
+
+        public void run()
+        {
+
+            using (var ws = CreateWebSocket("localhost", 46495))
             {
-                ws.OnMessage += (sender, e) =>
-                    Console.WriteLine("Pede *Randal says: " + e.Data);
+                ws.OnMessage += (sender, e) => OnMessageReaction(sender, e);
 
                 ws.Connect();
+                ws.Send("checkConnection");
+
+
                 ws.Send("BALUS");
                 Console.ReadKey(true);
+            }
+
+        }
+
+        private WebSocket CreateWebSocket(string ip, int port)
+        {
+            return new WebSocket("ws://" + ip + ":" + port + "/Chat");
+        }
+
+        private void OnMessageReaction(Object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Pede *Randal says: " + e.Data);
+            if (e.GetType() == Type.GetType("String"))
+            {
+
             }
         }
     }
